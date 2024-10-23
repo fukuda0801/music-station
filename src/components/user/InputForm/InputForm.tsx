61ab2258f8f61cf370470b9056a43e4724a8fe99
fukuda0801 <nagisano0801@gmail.com>
2024-10-23 23:21:14 +0900

    feat: propsにReact Hook Formで管理するためのregisterを追加

--- src/components/user/InputForm/InputForm.tsx
+++ src/components/user/InputForm/InputForm.tsx
@@ -1,11 +1,11 @@
 import type { InputFormProps } from "@/types/userType";
 import styles from "./InputForm.module.scss";
 
-const InputForm = ({ label, name, type, error }: InputFormProps) => {
+const InputForm = ({ label, name, type, error, register }: InputFormProps) => {
   return (
     <div className={styles.inputContentGroup}>
       <label htmlFor={name}>{label}</label>
-      <input type={type} id={name} name={name} />
+      <input type={type} id={name} {...register} />
       <p className={styles.errorMessage}>{error}</p>
     </div>
   );
