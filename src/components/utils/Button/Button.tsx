4bb48ed273ae28356793aa1d69a65abbbb6b7877
fukuda0801 <nagisano0801@gmail.com>
2024-10-23 23:19:12 +0900

    feat: propsにdisabledを追加

--- src/components/utils/Button/Button.tsx
+++ src/components/utils/Button/Button.tsx
@@ -2,10 +2,21 @@ import type { ButtonProps } from "@/types/utilType";
 import classNames from "classnames";
 import styles from "./Button.module.scss";
 
-const Button = ({ type, label, handleClick, variant }: ButtonProps) => {
+const Button = ({
+  type,
+  label,
+  handleClick,
+  variant,
+  disabled,
+}: ButtonProps) => {
   const buttonClass = classNames(styles.button, styles[variant]);
   return (
-    <button type={type} onClick={handleClick} className={buttonClass}>
+    <button
+      type={type}
+      onClick={handleClick}
+      className={buttonClass}
+      disabled={disabled}
+    >
       {label}
     </button>
   );
