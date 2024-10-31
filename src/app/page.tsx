246f706997495f6907722bac33c8f3ae997bb29e
fukuda0801 <nagisano0801@gmail.com>
2024-10-31 23:46:34 +0900

    feat: topページスタイル作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,52 @@
-const TopPage = () => {
+import SongDetailLink from "@/components/top/SongDetailLink/SongDetailLink";
+import SongGroup from "@/components/top/SongGroup/SongGroup";
+import SongGroupTitle from "@/components/top/SongGroupTitle/SongGroupTitle";
+import { getSingleSongRanking } from "@/utils/api";
+import styles from "./page.module.scss";
+
+const TopPage = async () => {
+  // シングルランキング楽曲4件取得
+  const singleSong = await getSingleSongRanking(4);
   return (
-    <div>
-      <p>トップページ</p>
+    <div className={styles.top__content}>
+      <section className={styles.top__content_unit}>
+        <div className={styles.top__content_title}>
+          <SongGroupTitle title="シングルランキング" />
+          <SongDetailLink link="/" />
+        </div>
+        <SongGroup songs={singleSong} />
+      </section>
+
+      <section className={styles.top__content_unit}>
+        <div className={styles.top__content_title}>
+          <SongGroupTitle title="アルバムランキング" />
+          <SongDetailLink link="/" />
+          {/* アルバムランキング */}
+        </div>
+      </section>
+
+      <section className={styles.top__content_unit}>
+        <div className={styles.top__content_title}>
+          <SongGroupTitle title="アーティストランキング" />
+          <SongDetailLink link="/" />
+          {/* アーティストランキング */}
+        </div>
+      </section>
+
+      <section className={styles.top__content_unit}>
+        <div className={styles.top__content_title}>
+          <SongGroupTitle title="人気新着" />
+          <SongDetailLink link="/" />
+          {/* 人気新着 */}
+        </div>
+      </section>
+
+      <section className={styles.top__content_unit}>
+        <div className={styles.top__content_title}>
+          <SongGroupTitle title="ジャンル一覧" />
+          {/* ジャンル一覧 */}
+        </div>
+      </section>
     </div>
   );
 };
