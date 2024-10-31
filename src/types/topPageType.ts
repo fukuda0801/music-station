50681fa3fe3c3e5148beb4ee685286806ac956c5
fukuda0801 <nagisano0801@gmail.com>
2024-10-31 23:51:57 +0900

    feat: topページで使用するコンポーネントのpropsの型を作成

---
+++ src/types/topPageType.ts
@@ -0,0 +1,21 @@
+// SongGroupコンポーネントの型
+export interface SongGroupProps {
+  id: number;
+  title: string;
+  duration: number;
+  preview: string;
+  image: string;
+  artist: {
+    id: number;
+    name: string;
+    image: string;
+  };
+}
+
+// SongInfoコンポーネントの型
+export interface SongInfoProps {
+  id: number;
+  title: string;
+  image: string;
+  artist: string;
+}
