82c6edc6e14fe13d3c9451fb7ad6109182fa7402
fukuda0801 <nagisano0801@gmail.com>
2024-11-01 17:43:10 +0900

    feat: ランキングのトップ３に異なるスタイルが当たるよう調整

--- src/components/top/ArtistGroup/ArtistGroup.tsx
+++ src/components/top/ArtistGroup/ArtistGroup.tsx
@@ -5,13 +5,14 @@ import styles from "./ArtistGroup.module.scss";
 const ArtistGroup = ({ artists }: { artists: ArtistGroupProps[] }) => {
   return (
     <div className={styles.content}>
-      {artists.map((artist: ArtistGroupProps) => {
+      {artists.map((artist: ArtistGroupProps, index) => {
         return (
           <ArtistInfo
             key={artist.id}
             id={artist.id}
             name={artist.name}
             image={artist.image}
+            num={index + 1}
           />
         );
       })}
--- src/components/top/SongGroup/SongGroup.tsx
+++ src/components/top/SongGroup/SongGroup.tsx
@@ -2,16 +2,17 @@ import type { SongGroupProps } from "@/types/topPageType";
 import SongInfo from "../SongInfo/SonInfo";
 import styles from "./SongGroup.module.scss";
 
-// トップページにて4曲楽曲を表示するコンポーネント
+// 楽曲概要を表示するコンポーネント
 const SongGroup = ({ songs }: { songs: SongGroupProps[] }) => {
   return (
     <>
       <div className={styles.group}>
-        {songs.map((song) => {
+        {songs.map((song, index) => {
           return (
             <SongInfo
               key={song.id}
               id={song.id}
+              num={index + 1}
               title={song.title}
               artist={song.artist.name}
               image={song.image}
