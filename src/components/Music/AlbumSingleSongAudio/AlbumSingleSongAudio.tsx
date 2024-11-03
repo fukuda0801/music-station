086a9cb4a800ca4f9cd9db81e219e49f6be78f8f
fukuda0801 <nagisano0801@gmail.com>
2024-11-03 23:25:16 +0900

    feat: AlbumSingleSongAudioコンポーネント作成 アルバム詳細ページにて楽曲を再生するコンポーネント

---
+++ src/components/Music/AlbumSingleSongAudio/AlbumSingleSongAudio.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import StopCircleIcon from "@mui/icons-material/StopCircle";
+import { useEffect, useRef } from "react";
+import styles from "./AlbumSingleSongAudio.module.scss";
+
+const AlbumSingleSongAudio = ({
+  preview,
+  handlePlay,
+  handlePause,
+  isPlaying,
+}: any) => {
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  // 再生状態に応じてオーディオを再生、停止
+  useEffect(() => {
+    // オーディオが存在しているか
+    if (!audioRef.current) {
+      return;
+    }
+    // 現在の楽曲が再生中ならtrue
+    if (isPlaying) {
+      // オーディオ再生
+      audioRef.current.play();
+    } else {
+      // オーディオを停止し再生位置リセット
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+    }
+  }, [isPlaying]);
+
+  // 停止時にコンテキストの値をリセット
+  const handleEnded = () => {
+    handlePause();
+  };
+
+  return (
+    <>
+      <audio src={preview} ref={audioRef} onEnded={handleEnded} />
+      <div>
+        {preview ? (
+          <>
+            <PlayCircleIcon
+              onClick={() => handlePlay()}
+              style={{ pointerEvents: isPlaying ? "none" : "auto" }}
+              aria-label="playButton"
+              className={
+                isPlaying ? styles.content__nonactive : styles.content__active
+              }
+            />
+            <StopCircleIcon
+              onClick={() => handlePause()}
+              style={{ pointerEvents: isPlaying ? "auto" : "none" }}
+              aria-label="stopButton"
+              className={
+                isPlaying ? styles.content__active : styles.content__nonactive
+              }
+            />
+          </>
+        ) : (
+          <p>プレビューが読み込めません</p>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default AlbumSingleSongAudio;
