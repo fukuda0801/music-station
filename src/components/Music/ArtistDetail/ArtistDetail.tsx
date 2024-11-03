4d7c8596adfd13a8c975d95a93251b3b4d861f6e
fukuda0801 <nagisano0801@gmail.com>
2024-11-04 03:02:21 +0900

    feat: アーティスト詳細を表示するコンポーネント作成

---
+++ src/components/Music/ArtistDetail/ArtistDetail.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import styles from "./ArtistDetail.module.scss";
+
+const ArtistDetail = ({ artist }: any) => {
+  return (
+    <div className={styles.content__artist}>
+      <Image
+        src={artist.image}
+        alt={`${artist.name}の画像`}
+        width={150}
+        height={150}
+        priority
+      />
+      <p>{artist.name}</p>
+      <button type="button">
+        <FavoriteBorderIcon />
+        <span>お気に入り楽曲に追加</span>
+      </button>
+    </div>
+  );
+};
+
+export default ArtistDetail;
