d6907b050a7702d401f5b91cef600522c48d39a4
fukuda0801 <nagisano0801@gmail.com>
2024-11-04 03:01:28 +0900

    feat: アーティストの詳細を取得するapiを作成

---
+++ src/app/api/music/get_artist_detail/[id]/route.ts
@@ -0,0 +1,33 @@
+import { MusicParams } from "@/types/deezerType";
+import axios from "axios";
+import { NextResponse, type NextRequest } from "next/server";
+
+const deezerUrl = process.env.DEEZER_URL;
+
+export const GET = async (_request: NextRequest, { params }: MusicParams) => {
+  try {
+    const { id } = params;
+    const artist = await axios.get(`${deezerUrl}/artist/${id}`);
+
+    // データが返ってこなかった場合
+    if (!artist) {
+      return NextResponse.json({
+        message: "アーティスト情報が見つかりませんでした",
+      });
+    }
+
+    const result = {
+      id: artist.data.id,
+      name: artist.data.name,
+      image: artist.data.picture_big,
+    };
+
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "サーバーエラーが発生しました" },
+      { status: 500 }
+    );
+  }
+};
