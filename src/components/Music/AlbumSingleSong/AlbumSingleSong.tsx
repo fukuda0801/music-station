0f86715240296ab7da59d5b482f166c8cab2933d
fukuda0801 <nagisano0801@gmail.com>
2024-11-03 23:24:34 +0900

    feat: AlbumSingleSongコンポーネント作成 アルバムに含まれている楽曲１つを表示

---
+++ src/components/Music/AlbumSingleSong/AlbumSingleSong.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Link from "next/link";
+import styles from "./AlbumSingleSong.module.scss";
+import { useAlbumAudio } from "@/context/AlbumAudioContext";
+import AlbumSingleSongAudio from "../AlbumSingleSongAudio/AlbumSingleSongAudio";
+
+// アルバム収録楽曲1曲分のコンポーネント
+const AlbumSingleSong = ({ id, title, preview, num }: any) => {
+  // コンテキストからstateを呼び出す
+  const { currentlyPlayingId, setCurrentlyPlayingId } = useAlbumAudio();
+
+  // この楽曲が再生中がどうか返す
+  const isPlaying = currentlyPlayingId === id;
+
+  // 再生中の楽曲のidをstateに格納
+  const handlePlay = () => {
+    setCurrentlyPlayingId(id);
+  };
+
+  // 止めたらstateから削除
+  const handlePause = () => {
+    setCurrentlyPlayingId(null);
+  };
+  // 楽曲をナンバリングするための記述
+  const displayNum = num.toString().padStart(2, "0");
+
+  return (
+    <div className={styles.content__single}>
+      <AlbumSingleSongAudio
+        preview={preview}
+        handlePlay={handlePlay}
+        handlePause={handlePause}
+        isPlaying={isPlaying}
+      />
+      <div className={styles.content__single_detail}>
+        <p>
+          <Link href={`/song/${id}`}>
+            {displayNum}: {title}
+          </Link>
+        </p>
+        <button type="button">
+          <FavoriteIcon role="img" aria-hidden="false" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AlbumSingleSong;
